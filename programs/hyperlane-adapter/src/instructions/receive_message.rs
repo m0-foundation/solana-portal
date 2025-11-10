39fc9a472601746e084ad24b34ff399b5ce71da3
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-11-10 13:51:47 -0500

    update receive

--- programs/hyperlane-adapter/src/instructions/receive_message.rs
+++ programs/hyperlane-adapter/src/instructions/receive_message.rs
@@ -1,9 +1,6 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::{Mint, TokenInterface};
 use common::{
-    earn::{self, accounts::EarnGlobal, program::Earn},
-    portal,
-    wormhole_verify_vaa_shim::program::WormholeVerifyVaaShim,
+    portal::{self, program::Portal},
     AUTHORITY_SEED,
 };
 
@@ -37,23 +34,7 @@ pub struct ReceiveMessage<'info> {
     /// CHECK: Account does not hold data
     pub messenger_authority: AccountInfo<'info>,
 
-    #[account(
-        mut,
-        seeds = [GLOBAL_SEED],
-        seeds::program = earn::ID,
-        bump = m_global.bump,
-        has_one = m_mint,
-    )]
-    pub m_global: Account<'info, EarnGlobal>,
-
-    #[account(mut)]
-    pub m_mint: InterfaceAccount<'info, Mint>,
-
-    pub wormhole_verify_vaa_shim: Program<'info, WormholeVerifyVaaShim>,
-
-    pub earn_program: Program<'info, Earn>,
-
-    pub token_program: Interface<'info, TokenInterface>,
+    pub portal_program: Program<'info, Portal>,
 
     pub system_program: Program<'info, System>,
 }
@@ -67,15 +48,11 @@ impl ReceiveMessage<'_> {
     pub fn handler<'info>(ctx: Context<'_, '_, '_, 'info, ReceiveMessage<'info>>) -> Result<()> {
         portal::cpi::receive_message(
             CpiContext::new(
-                ctx.accounts.wormhole_verify_vaa_shim.to_account_info(),
+                ctx.accounts.portal_program.to_account_info(),
                 portal::cpi::accounts::ReceiveMessage {
-                    relayer: ctx.accounts.relayer.to_account_info(),
+                    sender: ctx.accounts.relayer.to_account_info(),
                     adapter_authority: ctx.accounts.hyperlane_adapter_authority.to_account_info(),
                     messenger_authority: ctx.accounts.messenger_authority.to_account_info(),
-                    m_global: ctx.accounts.m_global.to_account_info(),
-                    m_mint: ctx.accounts.m_mint.to_account_info(),
-                    earn_program: ctx.accounts.earn_program.to_account_info(),
-                    m_token_program: ctx.accounts.token_program.to_account_info(),
                     system_program: ctx.accounts.system_program.to_account_info(),
                 },
             )
