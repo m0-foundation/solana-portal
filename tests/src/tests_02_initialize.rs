7c5756a5b2338862a1f506257c7837293bb43a47
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-11-10 09:45:01 -0500

    update idl

--- tests/src/tests_02_initialize.rs
+++ tests/src/tests_02_initialize.rs
@@ -1,6 +1,6 @@
 use anchor_lang::AccountDeserialize;
 use anyhow::{Ok, Result};
-use common::{portal::accounts::MessengerGlobal, wormhole_adapter::accounts::WormholeGlobal};
+use common::{portal::accounts::PortalGlobal, wormhole_adapter::accounts::WormholeGlobal};
 use solana_sdk::pubkey::Pubkey;
 use std::{str::FromStr, vec};
 
@@ -30,7 +30,7 @@ fn test_03_check_globals() -> Result<()> {
         "3bhczvnEexwTjdwR8b1LFDh5beYb8CLYAXUukZR8ZNdy",
     )?)?;
 
-    let global_portal = MessengerGlobal::try_deserialize(&mut data_portal.as_slice())?;
+    let global_portal = PortalGlobal::try_deserialize(&mut data_portal.as_slice())?;
     let global_wh = WormholeGlobal::try_deserialize(&mut data_wh.as_slice())?;
 
     assert_eq!(global_portal.admin, global_wh.admin);
