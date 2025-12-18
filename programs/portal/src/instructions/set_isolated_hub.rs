8354ac5ccc1ff1c12739ebabd57355faa6c2cb7d
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-12-18 12:09:48 -0500

    Apply suggestion from @Oighty
    Co-authored-by: Jordan Hall <90539730+Oighty@users.noreply.github.com>

--- programs/portal/src/instructions/set_isolated_hub.rs
+++ programs/portal/src/instructions/set_isolated_hub.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 use crate::state::{PortalGlobal, GLOBAL_SEED};
 
 #[derive(Accounts)]
-pub struct SetIsolatedHub<'info> {
+pub struct EnableCrossSpokeTransfers<'info> {
     pub admin: Signer<'info>,
 
     #[account(
@@ -11,18 +11,15 @@ pub struct SetIsolatedHub<'info> {
         seeds = [GLOBAL_SEED],
         bump = portal_global.bump,
         has_one = admin,
+        constraint = portal_global.isolated_hub_chain_id.is_some()
     )]
     pub portal_global: Account<'info, PortalGlobal>,
 }
 
-impl SetIsolatedHub<'_> {
-    pub fn handler(ctx: Context<Self>, chain_id: Option<u32>) -> Result<()> {
-        msg!(
-            "Setting isolated hub chain ID: {:?} -> {:?}",
-            ctx.accounts.portal_global.isolated_hub_chain_id,
-            chain_id
-        );
-        ctx.accounts.portal_global.isolated_hub_chain_id = chain_id;
+impl EnableCrossSpokeTransfers<'_> {
+    pub fn handler(ctx: Context<Self>) -> Result<()> {
+        msg!("Transfers to other spoke chains enabled.");
+        ctx.accounts.portal_global.isolated_hub_chain_id = None;
         Ok(())
     }
 }
