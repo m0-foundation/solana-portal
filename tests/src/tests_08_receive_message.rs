3137a974613a315b3dabff382106b8a35e25f108
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-12-19 11:36:04 -0500

    test invalid peers and chains

--- tests/src/tests_08_receive_message.rs
+++ tests/src/tests_08_receive_message.rs
@@ -16,9 +16,8 @@ use wormhole_adapter::{
     instruction as wormhole_instruction, instructions::VaaBody,
 };
 
+use crate::util::constants::{ETHEREUM_WORMHOLE_TRANSCEIVER, M_MINT, SOLANA_CHAIN_ID};
 use crate::{get_rpc_client, get_signer};
-
-const M_MINT: Pubkey = pubkey!("mzerojk9tg56ebsrEAhfkyc9VgKjTW2zDqp6C5mhjzH");
 
 #[test]
 fn test_01_receive_index_wormhole() -> Result<()> {
@@ -27,51 +26,14 @@ fn test_01_receive_index_wormhole() -> Result<()> {
     let program = client.program(wormhole_adapter::ID)?;
 
     let message_id = [42u8; 32];
-    let payload = Payload {
-        header: PayloadHeader {
-            message_id,
-            payload_type: 1,
-            destination_chain_id: 1399811149,
-            destination_peer: portal::ID.to_bytes(),
-        },
-        data: PayloadData::Index(IndexPayload { index: 0 }),
-    };
-
-    let vaa = VaaBody {
-        timestamp: 0,
-        nonce: 0,
-        emitter_chain: 2, // Ethereum
-        emitter_address: [
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 99, 25, 106, 9, 21, 117, 173, 249, 158, 35, 6,
-            229, 233, 14, 11, 229, 21, 72, 65, // Wormhole Transceiver on Ethereum
-        ],
-        sequence: 0,
-        consistency_level: 0,
-        payload: payload.clone(),
-    };
-
+    let payload = create_default_payload(message_id, SOLANA_CHAIN_ID, portal::ID.to_bytes());
+    let vaa = create_default_vaa(2, ETHEREUM_WORMHOLE_TRANSCEIVER, payload.clone());
     let metas = require_metas(&payload.data, signer.pubkey(), None, Some(M_MINT), None)?;
 
-    // Relay message
-    // Bridge validation is skipped with skip-validation feature flag
+    // Relay message - Bridge validation is skipped with skip-validation feature flag
     let result = program
         .request()
-        .accounts(wormhole_accounts::ReceiveMessage {
-            relayer: signer.pubkey(),
-            wormhole_global: pda!(&[GLOBAL_SEED], &wormhole_adapter::ID),
-            portal_global: pda!(&[GLOBAL_SEED], &portal::ID),
-            wormhole_adapter_authority: pda!(&[AUTHORITY_SEED], &wormhole_adapter::ID),
-            portal_authority: pda!(&[AUTHORITY_SEED], &portal::ID),
-            message_account: pda!(&[MESSAGE_SEED, &message_id], &portal::ID),
-            guardian_set: pda!(
-                &[GUARDIAN_SET_SEED, &0u32.to_be_bytes()],
-                &CORE_BRIDGE_PROGRAM_ID
-            ),
-            guardian_signatures: Pubkey::default(),
-            wormhole_verify_vaa_shim: wormhole_verify_vaa_shim::ID,
-            portal_program: portal::ID,
-            system_program: system_program::ID,
-        })
+        .accounts(create_receive_message_accounts(signer.pubkey(), message_id))
         .args(wormhole_instruction::ReceiveMessage {
             vaa_body: vaa.to_bytes(),
             guardian_set_index: 0,
@@ -153,4 +115,182 @@ fn test_02_receive_index_hyperlane() -> Result<()> {
     ));
 
     Ok(())
+}
+
+#[test]
+fn test_03_receive_invalid_peer_address() -> Result<()> {
+    let signer = get_signer();
+    let client = Client::new(Cluster::Localnet, signer.clone());
+    let program = client.program(wormhole_adapter::ID)?;
+
+    let message_id = [42u8; 32];
+    let payload = create_default_payload(message_id, SOLANA_CHAIN_ID, portal::ID.to_bytes());
+    let vaa = create_default_vaa(2, [4; 32], payload.clone()); // Invalid emitter address
+    let metas = require_metas(&payload.data, signer.pubkey(), None, Some(M_MINT), None)?;
+
+    let result = program
+        .request()
+        .accounts(create_receive_message_accounts(signer.pubkey(), message_id))
+        .args(wormhole_instruction::ReceiveMessage {
+            vaa_body: vaa.to_bytes(),
+            guardian_set_index: 0,
+        })
+        .accounts(metas)
+        .send();
+
+    assert!(result.is_err());
+    let err = result.err().unwrap().to_string();
+    assert!(
+        err.contains("Error Code: InvalidPeer"),
+        "Invalid error: {}",
+        err
+    );
+
+    Ok(())
+}
+
+#[test]
+fn test_04_receive_invalid_peer_chain() -> Result<()> {
+    let signer = get_signer();
+    let client = Client::new(Cluster::Localnet, signer.clone());
+    let program = client.program(wormhole_adapter::ID)?;
+
+    let message_id = [42u8; 32];
+    let payload = create_default_payload(message_id, SOLANA_CHAIN_ID, portal::ID.to_bytes());
+    let vaa = create_default_vaa(2334, ETHEREUM_WORMHOLE_TRANSCEIVER, payload.clone()); // Invalid chain
+    let metas = require_metas(&payload.data, signer.pubkey(), None, Some(M_MINT), None)?;
+
+    let result = program
+        .request()
+        .accounts(create_receive_message_accounts(signer.pubkey(), message_id))
+        .args(wormhole_instruction::ReceiveMessage {
+            vaa_body: vaa.to_bytes(),
+            guardian_set_index: 0,
+        })
+        .accounts(metas)
+        .send();
+
+    assert!(result.is_err());
+    let err = result.err().unwrap().to_string();
+    assert!(
+        err.contains("Error Code: InvalidPeer"),
+        "Invalid error: {}",
+        err
+    );
+
+    Ok(())
+}
+
+#[test]
+fn test_05_receive_invalid_destination_chain() -> Result<()> {
+    let signer = get_signer();
+    let client = Client::new(Cluster::Localnet, signer.clone());
+    let program = client.program(wormhole_adapter::ID)?;
+
+    let message_id = [42u8; 32];
+    let payload = create_default_payload(message_id, SOLANA_CHAIN_ID + 1, portal::ID.to_bytes()); // Invalid chain ID
+    let vaa = create_default_vaa(2334, ETHEREUM_WORMHOLE_TRANSCEIVER, payload.clone());
+    let metas = require_metas(&payload.data, signer.pubkey(), None, Some(M_MINT), None)?;
+
+    let result = program
+        .request()
+        .accounts(create_receive_message_accounts(signer.pubkey(), message_id))
+        .args(wormhole_instruction::ReceiveMessage {
+            vaa_body: vaa.to_bytes(),
+            guardian_set_index: 0,
+        })
+        .accounts(metas)
+        .send();
+
+    assert!(result.is_err());
+    let err = result.err().unwrap().to_string();
+    assert!(
+        err.contains("Error Code: InvalidPeer"),
+        "Invalid error: {}",
+        err
+    );
+
+    Ok(())
+}
+
+#[test]
+fn test_06_receive_invalid_destination_peer() -> Result<()> {
+    let signer = get_signer();
+    let client = Client::new(Cluster::Localnet, signer.clone());
+    let program = client.program(wormhole_adapter::ID)?;
+
+    let message_id = [42u8; 32];
+    let payload = create_default_payload(message_id, SOLANA_CHAIN_ID, [2; 32]); // Invalid peer
+    let vaa = create_default_vaa(2334, ETHEREUM_WORMHOLE_TRANSCEIVER, payload.clone());
+    let metas = require_metas(&payload.data, signer.pubkey(), None, Some(M_MINT), None)?;
+
+    let result = program
+        .request()
+        .accounts(create_receive_message_accounts(signer.pubkey(), message_id))
+        .args(wormhole_instruction::ReceiveMessage {
+            vaa_body: vaa.to_bytes(),
+            guardian_set_index: 0,
+        })
+        .accounts(metas)
+        .send();
+
+    assert!(result.is_err());
+    let err = result.err().unwrap().to_string();
+    assert!(
+        err.contains("Error Code: InvalidPeer"),
+        "Invalid error: {}",
+        err
+    );
+
+    Ok(())
+}
+
+fn create_default_payload(
+    message_id: [u8; 32],
+    destination_chain_id: u32,
+    destination_peer: [u8; 32],
+) -> Payload {
+    Payload {
+        header: PayloadHeader {
+            message_id,
+            payload_type: 1,
+            destination_chain_id,
+            destination_peer,
+        },
+        data: PayloadData::Index(IndexPayload { index: 0 }),
+    }
+}
+
+fn create_default_vaa(emitter_chain: u16, emitter_address: [u8; 32], payload: Payload) -> VaaBody {
+    VaaBody {
+        timestamp: 0,
+        nonce: 0,
+        emitter_chain,
+        emitter_address,
+        sequence: 0,
+        consistency_level: 0,
+        payload,
+    }
+}
+
+fn create_receive_message_accounts(
+    signer_pubkey: Pubkey,
+    message_id: [u8; 32],
+) -> wormhole_accounts::ReceiveMessage {
+    wormhole_accounts::ReceiveMessage {
+        relayer: signer_pubkey,
+        wormhole_global: pda!(&[GLOBAL_SEED], &wormhole_adapter::ID),
+        portal_global: pda!(&[GLOBAL_SEED], &portal::ID),
+        wormhole_adapter_authority: pda!(&[AUTHORITY_SEED], &wormhole_adapter::ID),
+        portal_authority: pda!(&[AUTHORITY_SEED], &portal::ID),
+        message_account: pda!(&[MESSAGE_SEED, &message_id], &portal::ID),
+        guardian_set: pda!(
+            &[GUARDIAN_SET_SEED, &0u32.to_be_bytes()],
+            &CORE_BRIDGE_PROGRAM_ID
+        ),
+        guardian_signatures: Pubkey::default(),
+        wormhole_verify_vaa_shim: wormhole_verify_vaa_shim::ID,
+        portal_program: portal::ID,
+        system_program: system_program::ID,
+    }
 }
