ca857ffe365364c823b3855c3e7e913e0b6be4ef
Kurtis Streutker <kurtisstreutker@gmail.com>
2026-03-02 14:43:54 -0500

    create lut

--- cli/src/commands/mod.rs
+++ cli/src/commands/mod.rs
@@ -1,10 +1,12 @@
 mod common;
 mod evm_common;
+pub mod create_hyperlane_lut;
 pub mod send_evm_index;
 pub mod send_evm_token;
 pub mod send_index;
 pub mod send_token;
 
+pub use create_hyperlane_lut::*;
 pub use send_evm_index::*;
 pub use send_evm_token::*;
 pub use send_index::*;
